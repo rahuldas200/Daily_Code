3e2a64088fe4c31941e7c1e8808916fa86cc865d
Rahul Das <106074968+rahuldas200@users.noreply.github.com>
2023-09-12 01:39:09 +0530

    2D.cpp

--- Dynamic Allocation/2D.cpp
+++ Dynamic Allocation/2D.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+    int n , m;
+    cin>>n;
+    cin>>m;
+
+    //creating 2D array
+    int ** arr = new int* [n];
+    for(int i=0;i<n;i++){
+        arr[i] = new int[m];
+    }
+    // taking input
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    // printing the array
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    //heap memory free 
+
+    for(int i=0;i<n;i++){
+        delete[] arr[i];
+    }
+
+    delete []arr;
+
+}
