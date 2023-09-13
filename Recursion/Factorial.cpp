72dabd6e1217ed3c3771c2def76c2f3874023756
Rahul Das <106074968+rahuldas200@users.noreply.github.com>
2023-09-13 20:11:08 +0530

    Create Factorial.cpp

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int factorial (int n){
+    if(n == 0){
+        return 1;
+    }
+    int smallerProblem = factorial(n-1);
+    int biggerProblem = n* smallerProblem;
+    return biggerProblem;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans = factorial(n);
+    cout<<ans;
+}
