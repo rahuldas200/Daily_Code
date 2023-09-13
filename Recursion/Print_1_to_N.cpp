7a1c99fa512c0a4a83aac632a095d09b994fe103
Rahul Das <106074968+rahuldas200@users.noreply.github.com>
2023-09-13 21:01:05 +0530

    Update Print_1_to_N.cpp

--- Recursion/Print_1_to_N.cpp
+++ Recursion/Print_1_to_N.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 void print(int n){
+    //base condition
     if(n == 0){
         return ;
     }
+    // print n 
     cout<<n<<endl;
+    // recursive relation
     print(n-1);
 }
 int main() {
